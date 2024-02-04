79a70de31a578c6b8f9aa5ca3593d9d03177b021
Hasnaat <25773007+HasnaatCodes@users.noreply.github.com>
2024-02-04 13:35:27 +0000

    Implemented Special Price functionality. All tests passing

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -5,7 +5,9 @@ namespace CheckoutKata;
 public class Checkout : ICheckout
 {
     private readonly ProductDatabaseContext _dbContext;
-    private double _total = 0;
+    private int _total = 0;
+
+    private Dictionary<string, int> cart = new Dictionary<string, int>();
 
     public Checkout(ProductDatabaseContext dbContext)
     {
@@ -17,14 +19,42 @@ public class Checkout : ICheckout
     {
         var productDetails = _dbContext.Products.FirstOrDefault(x => x.Sku == item);
 
-        if (productDetails != null)
+        if (productDetails == null)
         {
-            _total += productDetails.RegularPrice;
+            return;
         }
-    }
+        
+        AddToCart(item);
 
-    public double GetTotalPrice()
+        if (productDetails.SpecialPrice > 0 && cart[item] == productDetails.QuantityRequiredForSpecialPrice)
+        {
+            ApplySpecialPrice(productDetails, item);
+            return;
+        }
+        
+        _total += productDetails.RegularPrice;
+    }
+    
+    public int GetTotalPrice()
     {
         return _total;
+    }
+
+    private void ApplySpecialPrice(Product product, string item)
+    {
+        _total -= product.RegularPrice * (cart[item] - 1);
+        _total += product.SpecialPrice;
+    }
+
+    private void AddToCart(string item)
+    {
+        if (cart.ContainsKey(item))
+        {
+            cart[item]++;
+        }
+        else
+        {
+            cart.Add(item, 1);
+        }
     }
 }
