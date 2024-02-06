0c6e1afb732d567f303a491713282624b9965a5d
Hasnaat <25773007+HasnaatCodes@users.noreply.github.com>
2024-02-06 22:41:47 +0000

    All tests passing

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -5,9 +5,8 @@ namespace CheckoutKata;
 public class Checkout : ICheckout
 {
     private readonly ProductDatabaseContext _dbContext;
-    private int _total = 0;
-
-    private Dictionary<string, int> cart = new Dictionary<string, int>();
+    private readonly Dictionary<string, int> _cart = new();
+    private int _total;
 
     public Checkout(ProductDatabaseContext dbContext)
     {
@@ -18,7 +17,6 @@ public class Checkout : ICheckout
     public void Scan(string item)
     {
         var productDetails = _dbContext.Products.FirstOrDefault(x => x.Sku == item);
-
         if (productDetails == null)
         {
             return;
@@ -26,35 +24,66 @@ public class Checkout : ICheckout
         
         AddToCart(item);
 
-        if (productDetails.SpecialPrice > 0 && cart[item] == productDetails.QuantityRequiredForSpecialPrice)
-        {
-            ApplySpecialPrice(productDetails, item);
-            return;
-        }
+        // var currentQuantity = _cart[item];
+        // if (currentQuantity >= productDetails.QuantityRequiredForSpecialPrice)
+        // {
+        //     ApplySpecialPrice(productDetails);
+        //     return;
+        // }
         
         _total += productDetails.RegularPrice;
     }
     
     public int GetTotalPrice()
     {
+        // Work out special prices 
+        List<string> currentItemsInCart = _cart.Keys.ToList();
+
+        foreach (var item in currentItemsInCart)
+        {
+            var currentQuantity = _cart[item];
+            var productDetail = _dbContext.Products.FirstOrDefault(x => x.Sku == item);
+            
+            // if there is a special offer we want to work out the special price
+            if (productDetail?.QuantityRequiredForSpecialPrice > 0 && _cart[item] >= productDetail.QuantityRequiredForSpecialPrice)
+            {
+                // scenario 1 - RQ = 2, items added = 2
+                // scenario 2 - RQ = 2, items added = 4
+                
+                if (_cart[item] % productDetail.QuantityRequiredForSpecialPrice == 0)
+                {
+                    var timesDiscountWillBeApplied = currentQuantity / productDetail.QuantityRequiredForSpecialPrice;
+                    _total -= currentQuantity * productDetail.RegularPrice;
+                    _total += productDetail.SpecialPrice * timesDiscountWillBeApplied;
+                }
+                else
+                {
+                    var timesDiscountWillBeApplied = currentQuantity / productDetail.QuantityRequiredForSpecialPrice; // 2
+                    var quantityBeingDiscounted = timesDiscountWillBeApplied * productDetail.QuantityRequiredForSpecialPrice;
+                    _total -= quantityBeingDiscounted * productDetail.RegularPrice;
+                    _total += productDetail.SpecialPrice * timesDiscountWillBeApplied;
+                }
+            }
+        }
+        
         return _total;
     }
 
-    private void ApplySpecialPrice(Product product, string item)
+    private void ApplySpecialPrice(Product product)
     {
-        _total -= product.RegularPrice * (cart[item] - 1);
+        _total -= product.RegularPrice * (_cart[product.Sku] - 1);
         _total += product.SpecialPrice;
     }
 
     private void AddToCart(string item)
     {
-        if (cart.ContainsKey(item))
+        if (_cart.ContainsKey(item))
         {
-            cart[item]++;
+            _cart[item]++;
         }
         else
         {
-            cart.Add(item, 1);
+            _cart.Add(item, 1);
         }
     }
 }
